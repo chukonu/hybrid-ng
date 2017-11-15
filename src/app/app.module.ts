436cc03e7f8e41624f62b05c790b5ffd24a51d39
Zulong Chen <zulong.chen@teleopti.com>
2017-11-15 10:56:45 +0800

    Bootstrapping AngularJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
+import * as angular from 'angular';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
+import { downgradeComponent } from '@angular/upgrade/static';
+import { AgentTableComponent } from './agent-table/agent-table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgentTableComponent
   ],
   imports: [
     BrowserModule
@@ -16,3 +22,11 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+angular.module('hybridApp', [])
+  .directive(
+    'agentTable',
+    downgradeComponent({ component: AgentTableComponent }) as angular.IDirectiveFactory
+  );
+
+angular.bootstrap(document.body, ['hybridApp'], { strictDi: true });

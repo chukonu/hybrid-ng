7def2c3ea44fd6d3cfefcc4bc38f160529927d09
Zulong Chen <zulong.chen@teleopti.com>
2017-11-15 10:56:28 +0800

    New component: AgentTable

---
+++ src/app/agent-table/agent-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-table',
+  templateUrl: './agent-table.component.html',
+  styleUrls: ['./agent-table.component.css']
+})
+export class AgentTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

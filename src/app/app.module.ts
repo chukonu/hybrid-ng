d35b5c3b9563e71f7c8878791b23c0d64e46950e
Zulong Chen <zulong.chen@teleopti.com>
2017-11-15 17:19:25 +0800

    Downgrade an Angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { downgradeComponent } from '@angular/upgrade/static';
+import { downgradeComponent, UpgradeModule } from '@angular/upgrade/static';
 import { AgentTableComponent } from './agent-table/agent-table.component';
+import { ApplicationRef } from '@angular/core/src/application_ref';
+import { AgentDetailComponent } from './agent-detail/agent-detail.component';
 
+angular.module('hybridApp', [])
+  .controller('ajs', function () {
+    this.message = 'AngularJS';
+  })
+  .directive(
+    'agentTable',
+    downgradeComponent({ component: AgentTableComponent }) as angular.IDirectiveFactory
+  );
+
+// angular.bootstrap(document.body, ['hybridApp'], { strictDi: true });
 
 @NgModule({
   declarations: [
@@ -16,17 +28,26 @@ import { AgentTableComponent } from './agent-table/agent-table.component';
     AgentTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+
+  // bootstrap: [
+  //   AppComponent,
+  //   AgentTableComponent
+  // ],
+  // bootstrap: [AppComponent],
+
+  entryComponents: [
+    AppComponent,
+    AgentTableComponent
+  ]
 })
-export class AppModule { }
-
-angular.module('hybridApp', [])
-  .directive(
-    'agentTable',
-    downgradeComponent({ component: AgentTableComponent }) as angular.IDirectiveFactory
-  );
-
-angular.bootstrap(document.body, ['hybridApp'], { strictDi: true });
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+  ngDoBootstrap(app: ApplicationRef) {
+    this.upgrade.bootstrap(document.body, ['hybridApp'], { strictDi: true });
+    app.bootstrap(AppComponent);
+  }
+}
